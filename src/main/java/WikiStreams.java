71e44629db73e49fee798f8e89ff764778211242
Oussama Sadeg <sadeg.oussama@hotmail.fr>
2019-09-27 11:26:36 +0200

    Envoi d'un flux de données à Kafka

--- src/main/java/WikiStreams.java
+++ src/main/java/WikiStreams.java
@@ -17,56 +17,32 @@ import java.net.*;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.LongSerializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 
 public class WikiStreams {
-    public static class MyMapper extends
-            Mapper<LongWritable, Text, Text, IntWritable> {
-        private Text word = new Text();
 
-        public void map(LongWritable key, Text value, Context context)
-                throws IOException, InterruptedException {
-            String line = value.toString();
-            StringTokenizer tokenizer = new StringTokenizer(line);
-            while (tokenizer.hasMoreTokens()) {
-                word.set(tokenizer.nextToken());
-                context.write(word, new IntWritable(1));
-            }
+    private final static String TOPIC = "WikiStreams";
+    private final static String BOOTSTRAP_SERVERS = "localhost:9092,localhost:9093,localhost:9094";
 
-        }
-    }
-    public static class MyReducer extends
-            Reducer<Text, IntWritable, Text, IntWritable> {
-        public void reduce(Text key, Iterable<IntWritable> values,
-                           Context context) throws IOException, InterruptedException {
-            int sum = 0;
-            for (IntWritable val : values) {
-                sum += val.get();
-            }
-            context.write(key, new IntWritable(sum));
-        }
-    }
     public static void main(String[] args) throws Exception {
 
-        String url = "https://stream.wikimedia.org/v2/stream/recentchange";
+        if (args.length == 0) {
 
-        URLConnection conn = new URL
-                ("https://stream.wikimedia.org/v2/stream/recentchange").openConnection();
+            runProducer(5);
 
-        BufferedReader reader = new BufferedReader
-                (new InputStreamReader(conn.getInputStream(), StandardCharsets.UTF_8));
+        } else {
 
-        String line;
+            runProducer(Integer.parseInt(args[0]));
 
-        while ((line = reader.readLine()) != null) {
-
-            System.out.println(line);
-            // 1. parse interesting lines according to
-            //    their prefix event, id, or data
-            // 2. write filtered data to the database
         }
 
-       /*
+        /*
         Job job = Job.getInstance(new Configuration());
         job.setJarByClass(WikiStreams.class);
         job.setOutputKeyClass(Text.class);
@@ -89,4 +65,67 @@ public class WikiStreams {
         */
 
     }
+
+    private static Producer<Long, String> createProducer() {
+
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+        props.put(ProducerConfig.CLIENT_ID_CONFIG, "WikiStreams");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, LongSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        return new KafkaProducer<>(props);
+
+    }
+
+    static void runProducer(final int sendMessageCount) throws Exception {
+
+        final Producer<Long, String> producer = createProducer();
+        long time = System.currentTimeMillis();
+        final CountDownLatch countDownLatch = new CountDownLatch(sendMessageCount);
+
+        try {
+
+            URLConnection conn = new URL("https://stream.wikimedia.org/v2/stream/recentchange").openConnection();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream(), StandardCharsets.UTF_8));
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+
+                // New Producer of Wikipedia
+                final ProducerRecord<Long, String> record = new ProducerRecord<>(TOPIC, time,line);
+
+                // Send async records to Kafka
+                producer.send(record, (metadata, exception) -> {
+
+                    long elapsedTime = System.currentTimeMillis() - time;
+
+                    if (metadata != null) {
+
+                        System.out.printf("sent record(key=%s value=%s) " +
+                                              "meta(partition=%d, offset=%d) time=%d\n",
+                                              record.key(), record.value(), metadata.partition(),
+                                              metadata.offset(), elapsedTime
+                        );
+
+                    } else {
+
+                        exception.printStackTrace();
+
+                    }
+
+                    countDownLatch.countDown();
+
+                });
+
+            }
+
+        } finally {
+
+            producer.flush();
+            producer.close();
+
+        }
+    }
+
 }
